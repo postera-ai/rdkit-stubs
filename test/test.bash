bf7ff6a351c587d1fcf22593d5eea19b282b3b28
Andrew Dirksen <andrew@dirksen.com>
2022-11-09 16:08:58 -0800

    also typecheck the stubs themselves during ci

--- test/test.bash
+++ test/test.bash
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
-cd "$(dirname "$0")"
+this_script="$(realpath "$0")"
 
+# typecheck and run tests in this directory
+cd  "$(dirname "$this_script")"
 pyright .
 mypy .
 pytest .
+
+# typecheck the stubs themselves
+cd  "$(dirname "$(dirname "$this_script")")"/rdkit-stubs
+pyright .
+mypy .
