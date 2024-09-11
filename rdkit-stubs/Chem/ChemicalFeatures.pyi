84f5a7c9ebc770faea1a8c4d88b13f3ecf599252
Keisuke Yanagisawa <yanagisawa@c.titech.ac.jp>
2024-09-11 03:31:48 +0000

    clarify return value of MCFF_GetFeaturesForMol()

--- rdkit-stubs/Chem/ChemicalFeatures.pyi
+++ rdkit-stubs/Chem/ChemicalFeatures.pyi
@@ -2,4 +2,6 @@ from rdkit.Chem.rdChemicalFeatures import *
 from rdkit.Chem.rdMolChemicalFeatures import *
 from rdkit.Chem import Mol
 
-def MCFF_GetFeaturesForMol(self, mol: Mol, includeOnly: str = "", confId: int = -1) -> tuple: ...
+def MCFF_GetFeaturesForMol(
+    self, mol: Mol, includeOnly: str = "", confId: int = -1
+) -> tuple[MolChemicalFeature]: ...
